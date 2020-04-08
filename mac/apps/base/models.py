b8aebe32095cafc78b2161e2a181eab0fd4bf93e
motakuk <matvey@amixr.io>
2020-04-09 01:09:52 +0300

    refresh migrations

--- mac/apps/base/models.py
+++ mac/apps/base/models.py
@@ -20,7 +20,6 @@ class User(AbstractBaseUser, PermissionsMixin):
     date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
     is_active = models.BooleanField(_('active'), default=True)
     is_staff = models.BooleanField(_('staff status'),default=False)
-    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
 
     city = models.TextField(
         choices=CITIES,
