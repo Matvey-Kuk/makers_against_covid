4d05c76fb5768420f7405456e60c39f452382e83
motakuk <matvey@amixr.io>
2020-04-15 23:23:43 +0300

    Logging for prod

--- mac/mac/settings_production.py
+++ mac/mac/settings_production.py
@@ -25,3 +25,20 @@ DATABASES = {
         },
     },
 }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+}
